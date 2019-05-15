f107fc1b4653205c9354dc43146570c4fe180541
rinshadka <rinshad55@gmail.com>
2019-05-15 07:17:20 -0500

    Add application and containers update script

---
+++ bin/update-application.sh
@@ -0,0 +1,13 @@
+export CCF_PATH=/opt/event-navigator-containers
+cd $CCF_PATH
+sudo git fetch
+sudo git checkout origin/master cf-hasura/etc/liquibase/nen.postgresql.sql
+sudo docker image rm --force $(docker image ls  |grep none |awk '{print $3}')
+sudo docker pull hasura/graphql-engine
+cd $CCF_PATH/cf-hasura
+sudo ccfmake stop 
+sudo ccfmake start
+cd $CCF_PATH/cf-ui
+sudo ccfmake stop
+sudo ccfmake build
+sudo ccfmake start
